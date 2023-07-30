3ffbc58e75df0f50389b0fb1cb6ad2d60962978f
isaul19 <saulhola348@gmail.com>
2023-07-30 09:33:54 -0500

    add migrations for posts, categories, category_post

--- database/migrations/2023_07_29_225502_create_posts_table.php
+++ database/migrations/2023_07_29_225502_create_posts_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -13,6 +14,14 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
+            $table->string("title", 2048);
+            $table->string("slug", 2048);
+            $table->string("thumbnail", 2048);
+            $table->longText("body");
+            $table->boolean("is_active");
+            $table->datetime("published_at");
+            // $table->foreignId("user_id")->references("id")->on("users");
+            $table->foreignIdFor(User::class, "user_id");
             $table->timestamps();
         });
     }
